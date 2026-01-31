d9dcd9c6e81d8a8ccb5808dbaa42037427950369
mdelacruzrobles <mdelacruzrobles@byupathway.edu>
2026-01-30 21:00:59 -0500

    week03

--- scripts/place.js
+++ scripts/place.js
@@ -2,10 +2,17 @@ document.getElementById("lastModified").innerHTML = "Last Modified: " + document
 const today = new Date();
 document.getElementById("currentyear").innerHTML = today.getFullYear();
 
-function windChillConversion(temperature, wind) {
-    let result = 13.12 + 0.6215 * temperature - 11.37 * (wind ** 0.16) + 0.3965 * (temperature * (wind ** 0.16));
-    return result
+const temperature = 32;
+const windSpeed = 10;
+
+function calculateWindChill(temp, wind) {
+  return 35.74 + 0.6215 * temp - 35.75 * wind ** 0.16 + 0.4275 * temp * wind ** 0.16;
 }
-let windChill = windChillConversion(0, 20);
-let rounded = Number(windChill.toFixed(1));
-document.getElementById("windChillValue").textContent = rounded;
+
+let windChill = "N/A";
+
+if (temperature <= 50 && windSpeed > 3) {
+  windChill = calculateWindChill(temperature, windSpeed).toFixed(1);
+}
+
+document.getElementById("windChillValue").textContent = windChill;
