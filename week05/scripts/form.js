da374c38fc67cb4e4b618edde55fed8e688e2db2
mdelacruzrobles <mdelacruzrobles@byupathway.edu>
2026-02-13 22:52:05 -0500

    week05

---
+++ week05/scripts/form.js
@@ -0,0 +1,38 @@
+document.getElementById("lastModified").innerHTML = "Last Modified: " + document.lastModified;
+const today = new Date();
+document.getElementById("currentyear").innerHTML = today.getFullYear();
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+const selectElement = document.getElementById("product")
+const newSelectOption = document.createElement("option");
+products.forEach(product => {
+    const newSelectOption = document.createElement("option");
+    newSelectOption.textContent = product.name;
+    newSelectOption.value = product.id;
+    selectElement.append(newSelectOption);
+})
